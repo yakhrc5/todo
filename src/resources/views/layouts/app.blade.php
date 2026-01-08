be567cd28c6a85a516be1fa4062ea0828f76ac5e
yamaguchi akira <yakhr4@gmail.com>
2026-01-08 21:43:34 +0900

    セットアップ　Bladeファイル、CSSファイル入力まで

---
+++ src/resources/views/layouts/app.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Todo</title>
+    <link rel="stylesheet" href="{{ asset('css/common.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
+    @yield('css')
+</head>
+
+<body>
+    <header class="header">
+        <div class="header__inner">
+            <a href="/" class="header__logo">
+                Todo
+            </a>
+        </div>
+    </header>
+    <main>
+        @yield('content')
+    </main>
+</body>
+
+</html>
