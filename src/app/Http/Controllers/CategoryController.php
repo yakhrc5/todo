ffc6349888afa9f03557901a79d5750bad168d20
yamaguchi akira <yakhr4@gmail.com>
2026-01-11 11:44:57 +0900

    3章　カテゴリの追加機能とバリデーションの設定

---
+++ src/app/Http/Controllers/CategoryController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Http\Requests\CategoryRequest;
+
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        $categories = Category::all();
+
+        return view('category', compact('categories'));
+    }
+
+    public function store(CategoryRequest $request)
+    {
+        $category = $request->only('name');
+        Category::create($category);
+
+        return redirect('/categories')->with('message',"カテゴリを作成しました");
+    }
+}
